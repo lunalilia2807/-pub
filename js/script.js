3fb83a16131637108b553682c7d05eabdd1ff826
lunalilia2807 <154578684+lunalilia2807@users.noreply.github.com>
2023-12-24 16:30:04 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 //constant
 var todaysDate = new Date();
 const ourAnniversary = new Date("2023-04-27")
-const ourDayWeMet = new Date("2023-01-30")
+const ourDayWeMet = new Date("2023-01-01")
 
 //select DOM
 outputInSeconds = document.querySelector('.seconds');
